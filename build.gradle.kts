5bf17d672e745150a56e3f6776deea66afe24a68
manju-devv <manjunathpurum@gmail.com>
2025-11-10 01:16:49 +0530

    added name and app icon

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
 }
 
 
@@ -12,5 +13,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
     }
 }
